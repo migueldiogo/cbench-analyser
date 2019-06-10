3171d14cc8eddf78c12499a2fbd99aace70027dd
Miguel Diogo <miguel.diogo@talkdesk.com>
2019-06-10 01:14:27 +0100

    WIP

--- lib/Analyser.py
+++ lib/Analyser.py
@@ -1,11 +1,13 @@
-import os
+from __future__ import division
 
+import os
 from models.DataEntry import DataEntry
 
 
 class Analyser:
-    def __init__(self, file):
-        self.file = file
+    def __init__(self, input_filename, output_filename, print_header=True):
+        self.input_filename = input_filename
+        self.output_filename = output_filename
         self.data = list()
         self.registry = dict()
         self.write_average_latency = float()
@@ -18,9 +20,16 @@ class Analyser:
         self.database = str()
         self.write_consistency = str()
         self.read_consistency = str()
+        self.ratio_read_latency_and_consistency_score = float()
+        self.ratio_write_latency_and_consistency_score = float()
+        self.consistency_score = int()
+        self.total_operations = 0
+        self.unavailable_service_counter = 0
+        self.availability_score = int()
+        self.print_header = print_header
 
     def prepare_file(self):
-        with open(self.file) as file:
+        with open(self.input_filename) as file:
             filename_array = os.path.basename(file.name).split("_")
             self.timestamp = int(filename_array[0])
             self.database = filename_array[1]
@@ -29,12 +38,17 @@ class Analyser:
 
             for line in file.readlines():
                 if line[0] != '[':
+                    self.total_operations = self.total_operations + 1
                     raw_args = [x.strip() for x in line.split(',')]
                     worker_type = DataEntry.WRITER if raw_args[0].split(':')[0] == "writer_id" else DataEntry.READER
                     worker_id = raw_args[0].split(':')[1]
                     args = [x.split(':')[1] for x in raw_args[1:len(raw_args):1]]
-                    entry = DataEntry(worker_type, worker_id, args[0], args[1], args[2])
-                    self.data.append(entry)
+
+                    if 'UNAVAILABLE' in args[2]:
+                        self.unavailable_service_counter = self.unavailable_service_counter + 1
+                    else:
+                        entry = DataEntry(worker_type, worker_id, args[0], args[1], args[2])
+                        self.data.append(entry)
                 else:
                     raw_args = [x.strip() for x in line.split(',')]
                     if raw_args[0] == "[READ]":
@@ -52,9 +66,36 @@ class Analyser:
                         elif raw_args[1] == "MaxLatency(us)":
                             self.write_max_latency = float(raw_args[2])
 
-    def run(self):
-        self.prepare_file()
+            self.data = sorted(self.data, key=lambda item: item.timestamp)
 
+    def write_header(self, file):
+        file.write("database, "
+                   "write_consistency, "
+                   "read_consistency, "
+                   "average_time_read, "
+                   "average_time_write, "
+                   "consistency_score, "
+                   "ratio_read_latency_and_consistency_score, "
+                   "ratio_write_latency_and_consistency_score, "
+                   "total_operations, "
+                   "availability_score\n")
+
+    def write_data(self, file):
+        file.write(
+            str(self.database) + "," +
+            str(self.write_consistency) + "," +
+            str(self.read_consistency) + "," +
+            str(self.read_average_latency) + "," +
+            str(self.write_average_latency) + "," +
+            str(self.consistency_score) + "," +
+            str(self.ratio_read_latency_and_consistency_score) + "," +
+            str(self.ratio_write_latency_and_consistency_score) + "," +
+            str(self.total_operations) + "," +
+            str(self.availability_score) + "\n"
+
+        )
+
+    def calculate(self):
         inconsistencies_reads_counter = 0
         total_reads = 0
         for entry in self.data:
@@ -65,56 +106,37 @@ class Analyser:
                 timestamp, version = self.registry.get(entry.key, (0, 0))
                 if int(entry.version) < int(version) and int(timestamp) < int(entry.timestamp):
                     inconsistencies_reads_counter = inconsistencies_reads_counter + 1
-                    print("key:" + entry.key + ", " +
-                          "timestamp_write:" + timestamp + "," +
-                          "timestamp_read:" + entry.timestamp + ","+
-                          "version_write:" + version + "," +
-                          "version_read:" + entry.version)
-
-
-        print("\nPERFORMANCE MEASUREMENTS")
-
-        print("(1)Average time to perform a read operation = " + str(self.read_average_latency))
-        #print("Read Min Latency = " + str(self.read_min_latency))
-        #print("Read Max Latency = " + str(self.read_max_latency))
-
-        print("(2)Average time to perform a write operation = " + str(self.write_average_latency))
-        #print("Write Min Latency = " + str(self.write_min_latency))
-        #print("Write Max Latency = " + str(self.write_max_latency))
-
-        print("\nCONSISTENCY MEASUREMENTS")
-
-        print("Inconsistent reads = "
-              + str(inconsistencies_reads_counter))
+                    print(entry)
 
         consistent_reads_counter = total_reads - inconsistencies_reads_counter
-        print("Consistent reads = "
-              + str(consistent_reads_counter))
-        print("Total reads = "
-              + str(total_reads))
 
         try:
-            ratio_consistent_read_total_reads = consistent_reads_counter / total_reads
+            self.ratio_read_latency_and_consistency_score = self.read_average_latency / (
+                    consistent_reads_counter / total_reads)
         except ZeroDivisionError:
-            ratio_consistent_read_total_reads = 0
-        print("(4)Ratio consistent reads / total reads = "
-              + str(ratio_consistent_read_total_reads))
+            self.ratio_read_latency_and_consistency_score = 0
         try:
-            ratio_average_r_latency_total_consistency_r_prob = self.read_average_latency/(consistent_reads_counter/total_reads)
+            self.ratio_write_latency_and_consistency_score = self.write_average_latency / (
+                    consistent_reads_counter / total_reads)
         except ZeroDivisionError:
-            ratio_average_r_latency_total_consistency_r_prob = 0
-        print("(5)Ratio average read latency / total inconsistency read probability = "
-              + str(ratio_average_r_latency_total_consistency_r_prob))
-        try:
-            ratio_average_w_latency_total_inconsistency_r_prob = self.write_average_latency / (consistent_reads_counter / total_reads)
-        except ZeroDivisionError:
-            ratio_average_w_latency_total_inconsistency_r_prob = 0
-        print("(6)Ratio average write latency / total consistency read probability = "
-              + str(ratio_average_w_latency_total_inconsistency_r_prob))
+            self.ratio_write_latency_and_consistency_score = 0
 
         if consistent_reads_counter == 0:
-            consistency_score = 0
+            self.consistency_score = 0
         else:
-            consistency_score = consistent_reads_counter / total_reads * 100
+            self.consistency_score = consistent_reads_counter / total_reads * 100
 
-        print("Consistency score = " + str(consistency_score))
+        if self.unavailable_service_counter == self.total_operations:
+            self.availability_score = 0
+        else:
+            self.availability_score = (self.total_operations - self.unavailable_service_counter) / self.total_operations * 100
+
+    def run(self):
+        self.prepare_file()
+        self.calculate()
+
+        with open(self.output_filename, "a+") as file:
+            if self.print_header:
+                self.write_header(file)
+
+            self.write_data(file)
--- models/DataEntry.py
+++ models/DataEntry.py
@@ -8,3 +8,7 @@ class DataEntry:
         self.key = key
         self.timestamp = timestamp
         self.version = version
+
+    def __repr__(self):
+        return "{ timestamp:" + str(self.timestamp) + ", worker_type:" + str(self.worker_type) + str(self.worker_id) +\
+               ", key:" + str(self.key) + ", version:" + str(self.version) + " }"
